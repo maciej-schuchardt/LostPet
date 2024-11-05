6eee870860bd18f3847b2400449817af1a6554be
Maciej <ciekmax@gmail.com>
2024-11-05 19:54:57 +0100

    feat: add UserIdentityProcessor

---
+++ Services/UserIdentityProcessor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace LostPet.Services
+{
+    public class UserIdentityProcessor(AuthenticationStateProvider authenticationStateAsync)
+    {
+        private readonly AuthenticationStateProvider _authenticationStateAsync = authenticationStateAsync;
+
+        public async Task<string?> GetCurrentUserId()
+        {
+            var authstate = await this._authenticationStateAsync.GetAuthenticationStateAsync();
+
+            if (authstate == null)
+            {
+                return null;
+            }
+
+            var user = authstate.User;
+
+            return user.FindFirst(u => u.Type.Contains("nameidentifier"))?.Value;
+        }
+    }
+}
