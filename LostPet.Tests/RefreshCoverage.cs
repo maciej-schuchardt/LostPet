9617df5e528eac0ec5841bac29d06dec4cd617d1
Maciej <ciekmax@gmail.com>
2024-11-19 15:52:40 +0100

    tests: add RefreshCoverageTests

---
+++ LostPet.Tests/RefreshCoverage.cs
@@ -0,0 +1,12 @@
+namespace LostPet.Tests
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class RefreshCoverage
+    {
+        [Test]
+        public void RefreshCoverageTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
