47bd093af54f3d43530893035b89ee13fe81a013
Maciej <ciekmax@gmail.com>
2024-11-19 15:52:53 +0100

    tests: add PetService tests

---
+++ LostPet.Tests/PetServiceTests/PetService.tests.cs
@@ -0,0 +1,125 @@
+using System.Diagnostics.CodeAnalysis;
+using LostPet.Models;
+using LostPet.Tests.Helpers;
+
+namespace LostPet.Tests.PetServiceTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public partial class PetServiceTests
+    {
+
+        #region AddAsync Tests
+
+        [Test]
+        public async Task AddAsync_ShouldAddNewRecord()
+        {
+            // Arrange
+            Pet pet = new()
+            {
+                Name = "TestPet",
+                Species = "TestSpecies",
+                Breed = "TestBreed",
+                Color = "TestColor",
+                Age = new Random().Next(0, 40),
+                Weight = new Random().Next(2, 60),
+                MicrochipID = new Random().Next(1000000, 9999999).ToString(),
+                Photo = new Guid().ToString(),
+                Status = (Status)new Random().Next(0, 2),
+                UserID = new Guid().ToString(),
+                Description = "TestDescription",
+                LastSeenLocation = "TestLocation",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+            var recordsBeforeAdd = context.Pets.Count();
+
+            // Act
+            await petService.AddAsync(pet);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(context.Pets.Count(), Is.GreaterThan(recordsBeforeAdd));
+                Assert.That(context.Pets.Any(p => p == pet), Is.True);
+            });
+        }
+
+        [Test]
+        public void AddAsync_ShouldThrowExceptionOnInvalidObject()
+        {
+            // Act Assert
+            Assert.That(async delegate { await petService.AddAsync(new()); }, ThrowsHelper.DbUpdateException);
+        }
+
+        #endregion
+
+        [Test]
+        public async Task GetByIdAsync_ShouldReturnRequestedRecord()
+        {
+            TestContext.Out.WriteLine($"testname: {TestContext.CurrentContext.Test.Name} | randomId: {randomId}");
+            // Act
+            Pet result = await petService.GetByIdAsync(randomId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(context.Pets.Single(p => p.PetID == randomId)));
+        }
+
+        #region RemoveByIdAsync Tests
+
+        [Test]
+        public async Task RemoveByIdAsync_ShouldRemoveRecord()
+        {
+            // Arrange
+            TestContext.Out.WriteLine($"randomId: {randomId}");
+            int countBeforeRemove = context.Pets.Count();
+
+            // Act
+            await petService.RemoveByIdAsync(randomId);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(context.Pets.Count(), Is.LessThan(countBeforeRemove));
+                Assert.That(delegate { context.Pets.Single(p => p.PetID == randomId); }, Throws.InvalidOperationException);
+            });
+        }
+
+        [Test]
+        public void RemoveByIdAsync_ShouldThrowOnInvalidId()
+        {
+            Assert.That(async delegate { await petService.RemoveByIdAsync(invalidRandomId); }, Throws.InvalidOperationException);
+        }
+
+        #endregion
+
+        #region SetFoundByIdAsync Tests
+
+        [Test]
+        public async Task SetFoundByIdAsync_ShouldSetPetAsFound()
+        {
+            // Arrange
+            var pet = (await petService.GetByIdAsync(randomId)).Clone() as Pet;
+
+            // Act
+            await petService.SetFoundByIdAsync(randomId);
+            var petAfterChange = await petService.GetByIdAsync(randomId);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(pet.Status, Is.Not.EqualTo(Status.Found));
+                Assert.That(petAfterChange.Status, Is.Not.EqualTo(pet.Status));
+                Assert.That(petAfterChange.Status, Is.EqualTo(Status.Found));
+            });
+        }
+
+        [Test]
+        public void SetFoundByIdAsync_ShouldThrowOnInvalidId()
+        {
+            Assert.That(async delegate { await petService.SetFoundByIdAsync(invalidRandomId); }, Throws.InvalidOperationException);
+        }
+
+        #endregion
+    }
+}
